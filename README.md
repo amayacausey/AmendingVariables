3fecfd21cf7ec69682f84b28825ad65b9fb59425
amayacausey <amayacausey@gmail.com>
2020-06-22 11:48:51 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"#Amending Variables" 
---
+++ src/AmendingVariables.java
@@ -0,0 +1,19 @@
+public class AmendingVariables {
+    public static void main(String [] args){
+        int annualIncome = 62000;
+        double rate = .10;
+        int year = 5;
+        int annualIncome2;
+        double year5income;
+
+        annualIncome2=annualIncome*year;
+        year5income=rate*annualIncome2;
+
+        System.out.println("After 5 years your income will be : $" +year5income);
+
+
+
+
+
+    }
+}
