e02daa2edd5ca8754697f254e1c84d4db5e50ef5
amayacausey <amayacausey@gmail.com>
2020-06-23 10:38:17 -0700

    fixed calculations

--- src/AmendingVariables.java
+++ src/AmendingVariables.java
@@ -4,13 +4,13 @@ public class AmendingVariables {
         double rate = .10;
         int year = 5;
         int annualIncome2;
-        double year5income;
+        double year5rate,year5income;
 
         annualIncome2=annualIncome*year;
-        year5income=rate*annualIncome2;
+        year5rate=rate*annualIncome2;
+        year5income=year5rate+annualIncome;
 
         System.out.println("After 5 years your income will be : $" +year5income);
-
 
 
 
